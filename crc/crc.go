1f9487d58f5890109acce31a374033af522bfe07
Jaywalker <justin@justinandyadi.com>
2024-09-18 10:56:11 -0500

    Fixed CRC to use 32 bit int

--- crc/crc.go
+++ crc/crc.go
@@ -1,17 +1,17 @@
 package crc
 
 type CRC struct {
-	m_dwCRC32 [256]uint64
+	m_dwCRC32 [256]uint32
 }
 
-func (this *CRC) Calculate(data []byte, size uint64) uint64 {
-	var dwSize uint64
+func (this *CRC) Calculate(data []byte, size uint32) uint32 {
+	var dwSize uint32
 	dwSize = size
 	pData := make([]byte, len(data))
 	copy(pData, data)
-	var dwCrc uint64
+	var dwCrc uint32
 	dwCrc = 0
-	var k uint64
+	var k uint32
 	k = 0
 
 	// This is where the CRC lives. Null it out before we calculate it
@@ -21,20 +21,20 @@ func (this *CRC) Calculate(data []byte, size uint64) uint64 {
 	pData[9] = 0x00
 
 	for k = 0; k < dwSize; k++ {
-		dataByteAsUint := uint64(pData[k])
+		dataByteAsUint := uint32(pData[k])
 		dwCrc = this.m_dwCRC32[(dwCrc&0xFF)^dataByteAsUint] ^ (dwCrc >> 8)
 	}
 	return dwCrc
 }
 
 func New() (ret *CRC) {
-	var m_dwCRC32 [256]uint64
+	var m_dwCRC32 [256]uint32
 	// Init our CRC values
-	var m uint64
+	var m uint32
 	m = 0
 	for m = 0; m <= 255; m++ {
 		v1 := m
-		var bit uint64
+		var bit uint32
 		bit = 0
 		for bit = 8; bit > 0; bit-- {
 			if v1&1 != 0 {
